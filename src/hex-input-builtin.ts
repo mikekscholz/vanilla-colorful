83d3faabde6eff660b4790e3cb258b980d0a46b1
mikekscholz <mikekscholz@gmail.com>
2023-08-20 02:02:18 -0400

    Add files via upload

---
+++ src/hex-input-builtin.ts
@@ -0,0 +1,19 @@
+import { HexInputBuiltinBase } from './lib/entrypoints/hex-input-builtin.js';
+
+/**
+ * A custom element for entering color in HEX format.
+ *
+ * @element hex-input
+ *
+ * @prop {string} color - Color in HEX format.
+ * @attr {string} color - Selected color in HEX format.
+ * @prop {boolean} alpha - When true, `#rgba` and `#rrggbbaa` color formats are allowed.
+ * @attr {boolean} alpha - Allows `#rgba` and `#rrggbbaa` color formats.
+ * @prop {boolean} prefixed - When true, `#` prefix is displayed in the input.
+ * @attr {boolean} prefixed - Enables `#` prefix displaying.
+ *
+ * @fires color-changed - Event fired when color is changed.
+ */
+export class HexInputBuiltin extends HexInputBuiltinBase {}
+
+customElements.define('hex-input', HexInputBuiltin, {extends: "input"});
